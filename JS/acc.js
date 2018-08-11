b5e0d526aa6493cac4c926c60e4ec011a851b5bb
log1k <log1k@mail.ru>
2018-08-11 21:39:44 +0300

    accordion

--- JS/acc.js
+++ JS/acc.js
@@ -1,9 +1,26 @@
 function addonacc(c, ca) {
   const acc = document.querySelector(c);
   acc.addEventListener('click', function (e) {
+    
+    let sumActive = document.getElementsByClassName(ca);
+    
     if(e.target.tagName === "A") {
       e.preventDefault();
-      e.target.classList.toggle(ca);
+      e.stopPropagation();
+    if (e.target.classList.contains(ca)) {
+      e.target.classList.remove(ca);
+      //
+    } else {
+      for (let i = 0; i < sumActive.length; i++) {
+        sumActive[i].classList.remove(ca);
+      }
+      e.target.classList.add(ca)
+      //
+    }
+    } else {
+      for(let i of sumActive) {
+        i.classList.remove(ca);
+      }
     }
 })
 }
